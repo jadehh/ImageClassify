9cdf3cf4ab4ba2c8832326ca01c5876f8cadf6ca
jade <jadehh@live.com>
2019-09-17 10:58:51 +0800

    自定义数据的读取

--- dataset/sdfgoods.py
+++ dataset/sdfgoods.py
@@ -1,22 +1,9 @@
-# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-"""Provides data for the flowers dataset.
-
-The dataset scripts used to create the dataset can be found at:
-tensorflow/models/research/slim/datasets/download_and_convert_flowers.py
-"""
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 作者：2019/9/17 by jade
+# 邮箱：jadehh@live.com
+# 描述：自定义数据的读取
+# 最近修改：2019/9/17  上午10:40 modify by jade
 
 from __future__ import absolute_import
 from __future__ import division
@@ -25,7 +12,7 @@ from __future__ import print_function
 import os
 import tensorflow as tf
 
-from datasets import dataset_utils
+from dataset import dataset_utils
 
 slim = tf.contrib.slim
 from jade import *
