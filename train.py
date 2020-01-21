0e9f0e5b0fe5a329c49d55bc61878d08bcf02ac9
jade <jadehh@live.com>
2020-01-21 16:24:30 +0800

    update train file

--- train.py
+++ train.py
@@ -14,6 +14,8 @@ import tensorflow as tf
 from tensorflow.contrib import slim
 
 tf.logging.set_verbosity(tf.logging.INFO)
+
+
 class TrainModel():
     def __init__(self, args):
         self.model_name = args.model_name
@@ -34,18 +36,12 @@ class TrainModel():
                                                   "use_checkpoint" + "_" + GetToday()
                                                   )
 
-
         CreateSavePath(self.train_save_model_path)
         self.init_deploy_config()
         self.create_global_step()
         self.init_dataset()
         self.init_net()
         self.init_optimizer()
-
-
-
-
-
 
     def configure_learning_rate(self, num_samples_per_epoch, glbal_step):
         """
@@ -87,29 +83,45 @@ class TrainModel():
         :param self:
         :return:
         """
+        """Returns a function run by the chief worker to warm-start the training.
+
+        Note that the init_fn is only run when initializing the model during the very
+        first global step.
+
+        Returns:
+          An init function run by the supervisor.
+        """
         if self.checkpoint_path is None:
             return None
+
+        # Warn the user if a checkpoint exists in the train_dir. Then we'll be
+        # ignoring the checkpoint anyway.
         if tf.train.latest_checkpoint(self.train_save_model_path):
-            assert ValueError("不需要预训练模型了")
+            tf.logging.info(
+                'Ignoring --checkpoint_path because a checkpoint already exists in %s'
+                % self.train_save_model_path)
             return None
+
         exclusions = []
         if self.checkpoint_exclude_scopes:
-            exclusions = [scope.strip() for scope in self.checkpoint_exclude_scopes.split(",")]
+            exclusions = [scope.strip()
+                          for scope in self.checkpoint_exclude_scopes.split(',')]
 
+        # TODO(sguada) variables.filter_variables()
         variables_to_restore = []
         for var in slim.get_model_variables():
             for exclusion in exclusions:
                 if var.op.name.startswith(exclusion):
                     break
-                else:
-                    variables_to_restore.append(var)
+            else:
+                variables_to_restore.append(var)
 
         if tf.gfile.IsDirectory(self.checkpoint_path):
             checkpoint_path = tf.train.latest_checkpoint(self.checkpoint_path)
         else:
             checkpoint_path = self.checkpoint_path
 
-        print("fine-tuning from %s" % checkpoint_path)
+        tf.logging.info('Fine-tuning from %s' % checkpoint_path)
 
         return slim.assign_from_checkpoint_fn(
             checkpoint_path,
@@ -133,7 +145,7 @@ class TrainModel():
         #######################################################
 
         # select the dataset
-        print("load dataset",self.dataset_path)
+        print("load dataset", self.dataset_path)
         self.dataset = dataset_factory.get_dataset(
             self.dataset_name, self.dataset_path,
         )
@@ -162,10 +174,10 @@ class TrainModel():
             image = image_processing_fn(image, train_image_size, train_image_size)
 
             images, labels = tf.train.batch(
-            [image, label],
-            batch_size=self.batch_size,
-            num_threads=4,
-            capacity=5 * self.batch_size
+                [image, label],
+                batch_size=self.batch_size,
+                num_threads=4,
+                capacity=5 * self.batch_size
             )
             labels = slim.one_hot_encoding(labels, self.dataset.num_classes)
 
@@ -205,13 +217,12 @@ class TrainModel():
             self.optimizer,
             var_list=self.get_variables_to_train())
 
-
     def train(self):
         print("Start Training")
         first_clone_scope = self.deploy_config.clone_scope(0)
         update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS, first_clone_scope)
         grad_updates = self.optimizer.apply_gradients(self.clones_gradients,
-                                               global_step=self.global_step)
+                                                      global_step=self.global_step)
         update_ops.append(grad_updates)
 
         update_op = tf.group(*update_ops)
@@ -233,24 +244,23 @@ class TrainModel():
             save_interval_secs=600,
             sync_optimizer=None)
 
+    def clone_fn(self, batch_queue):
+        """Allows data parallelism by creating multiple clones of network_fn."""
+        images, labels = batch_queue.dequeue()
+        # """Build PNASNet Large model for the ImageNet Dataset."""
 
-    def clone_fn(self,batch_queue):
-      """Allows data parallelism by creating multiple clones of network_fn."""
-      images, labels = batch_queue.dequeue()
-      #"""Build PNASNet Large model for the ImageNet Dataset."""
-
-      logits, end_points = self.network_fn(images)
-      #############################
-      # Specify the loss function #
-      #############################
-      if 'AuxLogits' in end_points:
+        logits, end_points = self.network_fn(images)
+        #############################
+        # Specify the loss function #
+        #############################
+        if 'AuxLogits' in end_points:
+            slim.losses.softmax_cross_entropy(
+                end_points['AuxLogits'], labels,
+                label_smoothing=0.0, weights=0.4,
+                scope='aux_loss')
         slim.losses.softmax_cross_entropy(
-            end_points['AuxLogits'], labels,
-            label_smoothing=0.0, weights=0.4,
-            scope='aux_loss')
-      slim.losses.softmax_cross_entropy(
-          logits, labels, label_smoothing=0.0, weights=1.0)
-      return end_points
+            logits, labels, label_smoothing=0.0, weights=1.0)
+        return end_points
 
     def get_variables_to_train(self):
         """Returns a list of variables to train.
@@ -272,20 +282,26 @@ class TrainModel():
 
 if __name__ == '__main__':
     import argparse
+
     paraser = argparse.ArgumentParser(description="Classify")
     # genearl
     paraser.add_argument("--model_name", default="resnet_v2_101", help="model name")
     paraser.add_argument("--save_model_path",
-                         default="/home/jade/Models/Image_Classif/",
+                         default="/home/jade/sda2/Models/Image_Classif/",
                          help="path to save model")
-    paraser.add_argument("--dataset_name", default="sdfgoods",
+    paraser.add_argument("--dataset_name", default="car",
                          help="dataset_name")
-    paraser.add_argument("--dataset_path", default=GetRootPath() + "Data/DynamicFreezer/Tfrecords/dfgoods_bigger_2019-05-05_train.tfrecord", help="the dataset_path ")
-    paraser.add_argument("--checkpoint_path", default=None, help="the checkpoint_path ")
+    paraser.add_argument("--dataset_path",
+                         default="/home/jade/sda2/Data/CARDATA/Tfrecords/car_2020-01-21_train.tfrecord",
+                         help="the dataset_path ")
+    paraser.add_argument("--checkpoint_path",
+                         default="/home/jade/sda2/CheckPoint/resnet_v2_101_2017_04_14/resnet_v2_101.ckpt",
+                         help="the checkpoint_path ")
     paraser.add_argument("--image_size", default=128, help="the image_size")
     paraser.add_argument("--batch_size", default=32, help="the batch_size ")
-    paraser.add_argument("--checkpoint_exclude_scopes", default='resnet_v2_101/logits/biases,resnet_v2_101/logits/weights', help="the checkpoint_exclude_scopes ")
-
+    paraser.add_argument("--checkpoint_exclude_scopes",
+                         default='resnet_v2_101/logits/biases,resnet_v2_101/logits/weights',
+                         help="the checkpoint_exclude_scopes ")
 
     args = paraser.parse_args()
     linear = TrainModel(args)
